c48d10ccb96a19ba121844c45f81eaf7371e9847
EmersonLima1 <74200172+EmersonLima1@users.noreply.github.com>
2023-06-18 11:30:29 -0300

    update

--- previsao_partidas.py
+++ previsao_partidas.py
@@ -3843,7 +3843,7 @@ def times_acuracia(data_alvo, time_casa, time_fora, partidas_df):
     )
   
   #return informacoes_times
-  return ultima_partida, ultima_rodada, data_alvo
+  return times
     
 # Interação com o usuário
 
@@ -3931,15 +3931,13 @@ def main():
                         #df_tabela, df_legenda, df_casa, df_fora, df_res, df_inf = estilizar_df(df_concatenado_time_casa, df_concatenado_time_fora, df_resultados_confrontos_diretos, df_info_confrontos_diretos, time_casa_widget, time_fora_widget, tabela, legenda)
                         #_, df_novo = padroes_assertivos(partidas_df, data_da_partida, partidas_anteriores, multi_target_rfc, le, y_test)
                         #informacoes_times = times_acuracia(data_da_partida, time_casa_widget, time_fora_widget, partidas_df)
-                        ultima_partida, ultima_rodada, data_alvo = times_acuracia(data_widget, time_casa_widget, time_fora_widget, partidas_df)
+                        times = times_acuracia(data_widget, time_casa_widget, time_fora_widget, partidas_df)
                         st.header('**Previsões para a partida**')
                         st.subheader(f"{time_casa_widget} x {time_fora_widget}")
                         st.write(f'**Árbitro: {arbitro_widget}**')
                         st.write(f'**Data da partida: {data_widget}**')
 
-                        st.write(ultima_rodada)
-                        st.table(ultima_partida)
-                        st.write(data_alvo)
+                        st.table(times)
                         #st.table(df_tabela)
                         st.write('**Legenda dos Padrões**')
                         #st.table(df_legenda)
